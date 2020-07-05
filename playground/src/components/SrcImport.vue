fef184b6559ebbbace6e6f2a942a31d0f11febd0
likui <2218301630@qq.com>
2020-07-05 15:41:32 +0800

    feat(dev): support block src import handling

--- playground/src/components/SrcImport.vue
+++ playground/src/components/SrcImport.vue
@@ -1,12 +1,3 @@
-<template>
-  <div>
-    <h2>Src Import</h2>
-    <div>src import works! Import value is {{value}}.</div>
-  </div>
-</template>
-
+<template src="./srcImport.html"></template>
 <script src="./srcImport.ts" lang="ts"></script>
-
-<style lang="scss" scoped>
-
-</style>
+<style src="./srcImport.css" scoped></style>

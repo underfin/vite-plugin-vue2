94b3899a097332dbe1e53ece660d562e65cc4ec0
underfin <likui6666666@gmail.com>
2021-01-08 11:12:14 +0800

    fix(build): resolve template assets reference on build
    fix #15

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 const { createVuePlugin } = require('../dist')
 
 const config = defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [createVuePlugin({ jsx: true })],
 })
 

378622bd81c1178cf29873e481d80292f7857c91
underfin <2218301630@qq.com>
2021-03-18 23:43:52 +0800

    fix: use `esbuild` compile ts files
    fix https://github.com/underfin/vite-plugin-vue2/issues/49#issuecomment-799301238

--- playground/src-import/script.ts
+++ playground/src-import/script.ts
@@ -1,7 +1,9 @@
+const msg: string = 'hello from <script src="./script.ts">'
+
 export default {
   data() {
     return {
-      msg: 'hello from <script src="./script.ts">',
+      msg,
     }
   },
 }

cd171ec833d75dd762d6f020544e6277b1d8c730
underfin <2218301630@qq.com>
2021-08-09 22:23:17 +0800

    fix: support no-protocol asset url
    fix: #118

--- playground/test-assets/TestAssets.vue
+++ playground/test-assets/TestAssets.vue
@@ -16,6 +16,10 @@
       Absolute asset reference in template:
       <img src="/favicon.ico" style="width: 30px;" />
     </p>
+     <p>
+      Absolute asset reference without protocol header in the template: 
+      <img src="//cli.vuejs.org/favicon.png" style="width: 30px;" />
+    </p>
   </div>
 </template>
 

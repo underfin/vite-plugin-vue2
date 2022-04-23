dfc5970b7f93b8200643f5748842758e9e17bbd0
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-23 19:12:10 +0800

    chore: update deps

--- test/util.ts
+++ test/util.ts
@@ -1,6 +1,6 @@
 import fs from 'fs-extra'
 import path from 'path'
-import execa from 'execa'
+import { execa } from 'execa'
 import puppeteer, { ElementHandle } from 'puppeteer'
 
 let devServer: any
